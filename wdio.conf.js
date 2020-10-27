1d1acf1cec789cd4935114d9524e93711d7daac7
Vojtěch Červený <cervik49@gmail.com>
2020-10-27 15:42:57 +0100

    firefox setting

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,8 +52,9 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 1,
         //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
+        browserName: 'firefox',
+        // acceptInsecureCerts: true,
+        // headless: false,
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
@@ -106,8 +107,10 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
-    
+    //services: ['firefox-profile'],
+    hostname: 'localhost',
+    port: 4444,
+    path: '/wd/hub',
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks.html
